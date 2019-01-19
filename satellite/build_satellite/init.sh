34134d450c1c3de6bf167d31ac58ed076e8afa3a
parmstro@redhat.com <parmstro@parmstrong.ca>
2019-01-18 21:33:09 -0500

    Clean up hardcodes for Org

---
+++ satellite/build_satellite/init.sh
@@ -0,0 +1,22 @@
+# Get the hostname, IP Address, Netmask and Adapter
+# that we want the Satellite to run on. 
+# TODO: Support multiple network adapters for different services like PXE.
+
+# Get a valid FQDN for the host. We will be using this as the name for the REALM and domain for IdM
+# This is the one and only time to set the FQDN
+# 
+
+# Get a valid IP Address and Netmask from the user
+# This will be the base frontside address and will set the network that we communicate on.
+
+
+# Get the Adapter that we will apply this to. 
+# The hosts that we will build later need to PXE boot to this network.
+
+
+# Get a password for the Foreman Admin user
+
+# Get the manifest source location
+# this should be in the /root home directory
+
+# manifest_src_location
